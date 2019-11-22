68732b39cdc046ee927eaeb8594e9b075f1ff7bd
pumperknickle <53841072+pumperknickle@users.noreply.github.com>
2019-11-22 07:27:42 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-sudo docker run --net=asnet --ip=192.168.1.10 -v /Users/jbao/Honeypotlogs:/Honeyd/logs/ -p "445:445" -p "16992:16992" -p "16993:16993" honeypot
+sudo docker run -v /Users/jbao/Honeypotlogs:/Honeyd/logs/ -p "445:445" -p "16992:16992" -p "16993:16993" honeypot
